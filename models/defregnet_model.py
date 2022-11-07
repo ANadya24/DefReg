EPE calculation fix

models/defregnet_model.py
@@ -44,7 +44,19 @@ class DefRegNet(nn.Module):
         self.localization = self.localization.to(device)
         self.fc_loc = self.fc_loc.to(device)
         self.unet = self.unet.to(device)
-        self.spatial_transform = self.spatial_transform.to(device)
+        # self.spatial_transform = self.spatial_transform.to(device)
+        self.spatial_transform.device = device
+
+        
+    @property
+    def device(self):
+        return next(self.parameters()).device
+    
+    def to(self, device):
+        self.localization = self.localization.to(device)
+        self.fc_loc = self.fc_loc.to(device)
+        self.unet = self.unet.to(device)
+        self.spatial_transform.device = device
 
     def stn(self, x, y):
         """Spatial trasformer network."""

scripts/EPE_calculate_initial_baseline_errors.py
@@ -5,13 +5,19 @@ from skimage import io
 from glob import glob
 import pickle
 import sys
+import os
 
 from utils.ffRemap import dots_remap_bcw, ff_1_to_k
 from utils.points_error_calculation import frechetDist
 
 if __name__ == "__main__":
     prefix = 'fwd'
-    sequences = glob(sys.argv[1] + '/*.tif')
+    if sys.argv[1][-3:] == 'tif':
+        sequences = [sys.argv[1]]
+    else:
+        sequences = glob(sys.argv[1] + '/*.tif')
+        
+    os.makedirs(sys.argv[2], exist_ok=True)
     # going through sequences
     for seq_name in filter(lambda name: name.find('Seq') != -1, sequences):
         print(seq_name)
@@ -22,7 +28,8 @@ if __name__ == "__main__":
         poi = spio.loadmat(point_name)
 
         # load deformations from base elastic method
-        subf = ('/').join(seq_name.split('/')[:-1]) + '/deformations/numpy/'
+        subf = ('/').join(seq_name.split('/')[:-2]) + '/elastic_deformations/numpy/'
+        print(subf)
         baseline_def_name = subf + seq_name.split('/')[-1].split('.tif')[0] + f'_{prefix}.npy'
         base_deformation = np.load(baseline_def_name)
 

scripts/EPE_calculate_prediction_errors.py
@@ -8,16 +8,17 @@ import argparse
 
 from utils.ffRemap import dots_remap_bcw, ff_1_to_k
 from utils.points_error_calculation import frechetDist
-
+#TODO ADD THETAS
 
 def parse_args():
     parser = argparse.ArgumentParser(description='EPE script for drawing')
     parser.add_argument('--prefix', type=str, default='fwd',
                         help='prefix of deformation type')
     parser.add_argument('--sequence_path', type=str, default='fwd',
-                        help='path, where tif image sequneces are stored')
+                        help='whole sequence path or path, where tif image sequneces are stored')
     parser.add_argument('--seq_name_pattern', type=str, default='Seq',
                         help='path, where tif image sequneces are stored')
+    parser.add_argument('--prediction_path', type=str, help='predicted by model deformations')
     parser.add_argument('--save_pickle_path', type=str,
                         help='path to save calculated errors')
     args = parser.parse_args()
@@ -26,7 +27,11 @@ def parse_args():
 
 if __name__ == "__main__":
     args = parse_args()
-    sequences = glob(args.sequence_path + '/*.tif')
+    
+    if args.sequence_path[-3:] == 'tif':
+        sequences = [args.sequence_path]
+    else:
+        sequences = glob(args.sequence_path + '/*.tif')
     for seq_name in filter(lambda name: name.find(args.seq_name_pattern) != -1, sequences):
         print(seq_name)
         images = io.imread(seq_name).astype(np.float32)

scripts/EPE_vizualize_deformations.py
@@ -15,7 +15,7 @@ def parse_args():
     parser.add_argument('--prefix', type=str, default='fwd',
                         help='prefix of deformation type')
     parser.add_argument('--sequence_path', type=str, default='fwd',
-                        help='path, where tif image sequneces are stored')
+                        help='whole sequence path or path, where tif image sequneces are stored')
     parser.add_argument('--seq_name_pattern', type=str, default='Seq',
                         help='path, where tif image sequneces are stored')
     parser.add_argument('--prediction_path', type=str,
@@ -36,7 +36,10 @@ if __name__ == "__main__":
     if args.draw_points:
         os.makedirs(args.save_drawing_path, exist_ok=True)
 
-    sequences = glob(args.sequence_path + '/*.tif')
+    if args.sequence_path[-3:] == 'tif':
+        sequences = [args.sequence_path]
+    else:
+        sequences = glob(args.sequence_path + '/*.tif')
     for seq_name in filter(lambda name: name.find(args.seq_name_pattern) != -1, sequences):
         print(seq_name)
         images = io.imread(seq_name).astype(np.float32)