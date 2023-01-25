661a78977141887413bb665504e04585aae85d9f
ANadya24 <anshinan@yandex.ru>
2023-01-25 21:44:55 +0000

    Bug fixes and output fix

--- scripts/test_defregnet_w_points.py
+++ scripts/test_defregnet_w_points.py
@@ -3,6 +3,7 @@ sys.path.append('/srv/fast1/n.anoshina/DefReg/')
 from typing import cast
 import torch
 import pickle
+import numpy as np
 
 from models.defregnet_model import DefRegNet
 
@@ -10,6 +11,11 @@ from inference_config import InferenceConfig
 from src.config import load_yaml
 from inference_sequence import iterative_neigbours_predict, \
     collect_deformations_frame, apply_2nd_step_defs
+from utils.points_error_calculation import (
+    compute_l2_error_sequence, 
+    compute_frechet_error_sequence, 
+    load_points
+)
 
 
 if __name__ == '__main__':
@@ -24,20 +30,31 @@ if __name__ == '__main__':
 
     print("Model loaded successfully!")
 
-    for iterator_file, file in enumerate(config.image_sequences):
-        seq_name = file.split("/")[-1].split(".")[0]
+    for iterator_file, filename in enumerate(config.image_sequences):
+        seq_name = filename.split("/")[-1].split(".")[0]
+        print('#'*20)
         print(seq_name)
         with open(f'../data/point_dicts/elast_point_dict_{seq_name}.pkl', 'rb') as file:
             elast_data = pickle.load(file)
 
-        # with open(f'../data/point_dicts/init_point_dict_{seq_name}.pkl', 'rb') as file:
-        #     init_data = pickle.load(file)
+        with open(f'../data/point_dicts/init_point_dict_{seq_name}.pkl', 'rb') as file:
+            init_data = pickle.load(file)
+            
+        print('initial_error')
+        print(np.mean(compute_l2_error_sequence(init_data['bound'])))
+        print(np.mean(compute_l2_error_sequence(init_data['inner'])))
+        # print(np.mean(compute_frechet_error_sequence(init_data['lines'], init_data['line_length'])))
+        
+        print('elastic_error')
+        print(np.mean(compute_l2_error_sequence(elast_data['bound'])))
+        print(np.mean(compute_l2_error_sequence(elast_data['inner'])))
+        # print(np.mean(compute_frechet_error_sequence(init_data['lines'], init_data['line_length'])))
 
         print('iterative_neigbours_predict')
         model.use_theta = True
         prev_use_elastic = False
         iterative_neigbours_predict(iterator_file=iterator_file,
-                                    file=file, model=model,
+                                    file=filename, model=model,
                                     config=config, prev_use_elastic=prev_use_elastic,
                                     elast_data=elast_data, save_folder='initial_image_deformed')
 
